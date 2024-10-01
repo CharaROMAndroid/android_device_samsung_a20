82eca7a264b85c48049ad9cccc52f1856c24bd17
Ivy <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2024-10-01 15:10:43 -0230

    a20: set boot animation res

--- full_a20.mk
+++ full_a20.mk
@@ -28,3 +28,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="a20cis-user 11 RP1A.200720.012 A205FNXXSBCWC3 release-keys" \
     BuildFingerprint=samsung/a20cis/a20:11/RP1A.200720.012/A205FNXXSBCWC3:user/release-keys
+
+# crDroid build flags
+TARGET_BOOT_ANIMATION_RES := 720
+
