8b3fea5e5ada2dd4e7f7140191917e5c4504f0d4
Ivy <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2025-03-12 21:38:39 -0230

    device: we are not guacamoleb

--- device.mk
+++ device.mk
@@ -29,7 +29,7 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Quick Tap
 PRODUCT_COPY_FILES += \
-    device/oneplus/guacamoleb/conf/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
+    device/samsung/a20/conf/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.columbus.use_ap_sensor=false
 
