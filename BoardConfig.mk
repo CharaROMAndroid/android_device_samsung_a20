883d596caf0aa972f79bac8a85af43baa87babb7
royna2544 <whiteshell2544@naver.com>
2025-04-08 16:38:13 +0900

    a20: Move TARGET_SOC to device.mk
    - And set TARGET_DEVICE as well.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,9 +15,6 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4303355904
 BOARD_CACHEIMAGE_PARTITION_SIZE := 157286400
 BOARD_VENDORIMAGE_PARTITION_SIZE := 452984832
 
-# Target SoC
-TARGET_SOC := exynos7884B
-
 # Inherit common board flags
 include device/samsung/exynos7885-common/BoardConfigCommon.mk
 
