b51e49aa67f6bd9d40bd5707ea3055c169aee39d
nhglong <its.nhglong@gmail.com>
2025-09-17 13:50:25 +0000

    a20: Wire up PRODUCT_GMS_CLIENTID_BASE

--- lineage_a20.mk
+++ lineage_a20.mk
@@ -20,6 +20,8 @@ PRODUCT_MODEL := SM-A205F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="a20cis-user 11 RP1A.200720.012 A205FNXXSBCWC3 release-keys" \
     BuildFingerprint=samsung/a20cis/a20:11/RP1A.200720.012/A205FNXXSBCWC3:user/release-keys
