71f64ea058dd0b206e51d0a3ce38d995f645bf01
nhglong <its.nhglong@gmail.com>
2025-09-19 05:50:02 +0000

    a20: Sort

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,14 +1,17 @@
 DEVICE_PATH := device/samsung/a20
 
+# Inherit common board flags
+include device/samsung/exynos7885-common/BoardConfigCommon.mk
+
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := a20,a20dd
+
+# Display
+TARGET_SCREEN_DENSITY := 280
 
 # Kernel
 TARGET_KERNEL_CONFIG := exynos7885_defconfig
 TARGET_KERNEL_CONFIG += a20.config
-
-# Display
-TARGET_SCREEN_DENSITY := 280
 
 # Partitions
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 55574528 # 55MB
@@ -18,9 +21,6 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 452984832
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
-
-# Inherit common board flags
-include device/samsung/exynos7885-common/BoardConfigCommon.mk
 
 # SPL
 VENDOR_SECURITY_PATCH := 2021-06-01
